32625a51d310131e6011e10254520cc948e23712
salepopov <salepopov107@gmail.com>
2026-01-31 05:50:28 +0100

    Parallel logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use rand::Rng;
 use std::fs::File;
 use std::io::Write;
+use std::thread;
 
 fn main() {
-    sekvencijalno();
+    paralelno();
 }
 
 fn sekvencijalno() {
@@ -11,7 +12,7 @@ fn sekvencijalno() {
     let all_dots = 1000000;
 
     let mut file = File::create("izlaz_sekvenciajlni").expect("Ne mogu da kreiram fajl");
-    ;
+
     writeln!(file,"Trenutna procena PI:").expect("Ne moze da se upise");
 
     let mut inside_dot = 0;
@@ -43,5 +44,52 @@ fn sekvencijalno() {
 
 
 fn paralelno() {
+    let all_dots = 10000000;
+    let threads = 4;
+    let dots_per_threads = all_dots/threads;
 
+    let mut handles = vec![];
+
+    let mut file = File::create("izlaz_paralelni").expect("Ne mogu da kreiram fajl");
+
+    writeln!(file,"Trenutna procena PI:").expect("Ne moze da se upise");
+
+    let mut rng = rand::thread_rng();
+    let mut inside_dot = 0;
+
+    for _ in 1..=threads {
+        let handle = thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+
+            for _ in 0..dots_per_threads{
+                let x: f64 = rng.gen_range(-1.0..1.0);
+                let y: f64 = rng.gen_range(-1.0 .. 1.0);
+
+                let square_distance = x.powi(2) + y.powi(2);
+
+                if square_distance <= 1.0 {
+                    // println!("Tacka ({},{}) je UNUTAR kruga",x,y);
+                    inside_dot += 1;
+                } else {
+                    // println!("Tacka ({},{}) je VAN kruga",x,y)
+                }
+            }
+
+            inside_dot
+
+        });
+
+        handles.push(handle);
+
+    }
+
+    let mut total_hits = 0;
+    for handle in handles{
+        let hits = handle.join().expect("Nit je pukla");
+        total_hits += hits;
+    }
+
+    let estimate_pi = 4.0 * (total_hits as f64) / (all_dots as f64);
+
+    writeln!(file,"Nakon {} uzoraka, procena PI je {}",all_dots,estimate_pi).expect("Greska pri upisivanju.");
 }
