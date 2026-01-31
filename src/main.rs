23cb28a85ba8e6f9a2bea8a2d5713d0ca7598c38
salepopov <salepopov107@gmail.com>
2026-01-31 04:30:07 +0100

    Wrting in file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use rand::Rng;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
+    sekvencijalno();
+}
+
+fn sekvencijalno() {
     let mut rng = rand::thread_rng();
     let all_dots = 1000000;
 
+    let mut file = File::create("izlaz_sekvenciajlni").expect("Ne mogu da kreiram fajl");
+    ;
+    writeln!(file,"Trenutna procena PI:").expect("Ne moze da se upise");
+
     let mut inside_dot = 0;
 
-    for _ in 1..all_dots {
+    for i in 1..=all_dots {
         let x: f64 = rng.gen_range(-1.0..1.0);
         let y: f64 = rng.gen_range(-1.0 .. 1.0);
 
@@ -18,9 +28,20 @@ fn main() {
         } else {
             // println!("Tacka ({},{}) je VAN kruga",x,y)
         }
+
+        if i % 100000 == 0 {
+            let current_pi = 4.0 * (inside_dot as f64) / (all_dots as f64);
+            writeln!(file, "Na {} uzoraka, procena je {}", i, current_pi).expect("Greska pri upisivanju");
+        }
     }
 
     let estimate_pi = 4.0 * (inside_dot as f64) / (all_dots as f64);
 
     println!("Nakon {} uzoraka, procena PI je {}",all_dots,estimate_pi);
+}
+
+
+
+fn paralelno() {
+
 }
